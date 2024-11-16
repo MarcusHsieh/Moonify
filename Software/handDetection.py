60e7dce77928687b161297ca94d8300dac5deb6c
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2024-11-16 10:28:45 -0800

    Test open palm + fist

--- Software/handDetection.py
+++ Software/handDetection.py
@@ -1,22 +1,54 @@
 import cv2
 import mediapipe as mp
+import math
 
 # start mediapipe hand module
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
-# video capture!
-cap = cv2.VideoCapture(0)
+# dist calc
+def calculate_distance(point1, point2):
+    return math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2)
 
+# primary gesture detection func
 def detect_gesture(landmarks):
     thumb_tip = landmarks[4]
     index_tip = landmarks[8]
-    
-    # dist b/n thumb and index tip
-    if abs(thumb_tip.x - index_tip.x) < 0.05 and abs(thumb_tip.y - index_tip.y) < 0.05:
+    middle_tip = landmarks[12]
+    ring_tip = landmarks[16]
+    pinky_tip = landmarks[20]
+
+    index_base = landmarks[5]
+    middle_base = landmarks[9]
+    ring_base = landmarks[13]
+    pinky_base = landmarks[17]
+
+    # fingers all open + spread
+    is_open_palm = (
+        calculate_distance(index_tip, index_base) > 0.1 and
+        calculate_distance(middle_tip, middle_base) > 0.1 and
+        calculate_distance(ring_tip, ring_base) > 0.1 and
+        calculate_distance(pinky_tip, pinky_base) > 0.1
+    )
+
+    # fist
+    is_fist = (
+        calculate_distance(index_tip, index_base) < 0.05 and
+        calculate_distance(middle_tip, middle_base) < 0.05 and
+        calculate_distance(ring_tip, ring_base) < 0.05 and
+        calculate_distance(pinky_tip, pinky_base) < 0.05
+    )
+
+    if is_open_palm:
+        return "Next Song"
+    elif is_fist:
         return "Pause/Play"
-    return "No Gesture"
+    else:
+        return "No Gesture"
+
+# video capture!
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
